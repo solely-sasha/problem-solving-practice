4834e67a2a48367f902143c34d951ba7870768d5
sasha sims <solely.sashasims@gmail.com>
2022-12-06 14:14:42 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+//  1. understand the problem
+//
+
+// write a function that takes an array of numbers and returns the largest
+// the parameter is an array of numbers and the output will be the largest number in that array
+
+
+
+//  3. implement the plan
+
+function largest(array) {
+
+   
+}
+largest([60, 45, 3, 22, 8])
+//  4  review
