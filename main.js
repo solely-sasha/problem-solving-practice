67c0140d82db64d015070bc82ac3ac778b206868
sasha sims <solely.sashasims@gmail.com>
2022-12-07 17:52:38 -0500

    solved first challenge

--- main.js
+++ main.js
@@ -3,14 +3,28 @@
 
 // write a function that takes an array of numbers and returns the largest
 // the parameter is an array of numbers and the output will be the largest number in that array
+// use a for loop to loop over the numbers and find the largest number
 
 
 
 //  3. implement the plan
 
 function largest(array) {
+  
+    let highestNum = array[0];
+    for (var i = 0; i < array.length; i++){
+        let currentNum = array[i]
+        if(currentNum > highestNum){
+            highestNum = currentNum;
+            return highestNum;
+            
+        }
+    }
 
-   
+
 }
-largest([60, 45, 3, 22, 8])
+
+
 //  4  review
+// console.log(largest([4, 54, 28, 19, 23, 5, 6]))
+console.log(largest([36, 8, 40, 13, 9]))
